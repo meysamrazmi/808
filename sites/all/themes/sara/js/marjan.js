643535f0ae3dfebd012ecf76aec703ba1de5477c
meysamrazmi <m.razmi.92@gmail.com>
2017-05-15 15:49:12 +0430

    marjan modifications

--- sites/all/themes/sara/js/marjan.js
+++ sites/all/themes/sara/js/marjan.js
@@ -1,7 +1,10 @@
 var $ = jQuery;
 $(document).ready(function () {
-    /*for erasing pane-uc-order when its empty*/
+    /*erasing pane-uc-order when its empty*/
     if (!$('.page-user .pane-uc-orders-user .view-uc-orders-user div').length) {
         $('.pane-uc-orders-user').remove();
     }
+    /*adjust position of bookmark icon and admin-tabs in node-page*/
+    $('.page-node li.flag-bookmarks').css({'top':$('#page-title').height()-20});
+    $('.page-node div.tabs').css({'top':$('#page-title').height()});
 });
