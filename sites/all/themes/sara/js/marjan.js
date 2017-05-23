28dda0ec357c96cdb6668d3549a837909057ea89
meysamrazmi <m.razmi.92@gmail.com>
2017-05-23 19:03:53 +0430

    modifying some styles: css, less, js

--- sites/all/themes/sara/js/marjan.js
+++ sites/all/themes/sara/js/marjan.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     }
     
     /*adjust position of bookmark icon and admin-tabs in node-page*/
-    $('.page-node:not(.page-gallery):not(.page-shop):not(.page-pedia) .flag-outer-bookmarks').css({'top':$('#page-title').height()-20});
+    $('.page-node:not(.page-gallery):not(.page-shop):not(.page-pedia) .flag-outer-bookmarks').css({'top':$('#page-title').height()-23});
     
 //    $('.page-college .flag-outer-bookmarks').css({'right':$('#page-title').width()+ 5});
     
@@ -19,9 +19,11 @@ $(document).ready(function () {
     $('.node.node-product.view-mode-full .group-left > .display-price,.node.node-product-kit.view-mode-full .group-left > .display-price').prepend('<span>قیمت نهایی:</span>');
     
     /*ancher of pedia node*/
-    $('.page-node.page-pedia .view-mode-full .group-anchor > div').prepend('<div class="divanchors"></div>');
     
     if($('.page-node.page-pedia .field-type-node-reference').length){
+        $('.page-node.page-pedia .view-mode-full .group-anchor > div').prepend('<div class="divanchors"></div>');
+        $('.page-node.page-pedia .flag-outer-bookmarks').css({'top': 91});
+        
         $('.page-node.page-pedia .field-type-node-reference').each(function(){
             var anchclass = $(this).attr('class').split(" ")[1].substr(17);
             var spananch = $(this).find('.field-label').text().slice(0,-2);
@@ -33,6 +35,7 @@ $(document).ready(function () {
             
         });
     }
+    
     
     $('.divanchors a').click(function(e) {
         e.preventDefault();
