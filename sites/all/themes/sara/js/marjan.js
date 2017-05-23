7b0586984f34d35155444315cfe301b187a0e87e
meysamrazmi <m.razmi.92@gmail.com>
2017-05-23 10:56:58 +0430

    adding anchors to refrenced nodes for pedia pages

--- sites/all/themes/sara/js/marjan.js
+++ sites/all/themes/sara/js/marjan.js
@@ -4,7 +4,48 @@ $(document).ready(function () {
     if (!$('.page-user .pane-uc-orders-user .view-uc-orders-user div').length) {
         $('.pane-uc-orders-user').remove();
     }
+    
     /*adjust position of bookmark icon and admin-tabs in node-page*/
-    $('.page-node li.flag-bookmarks').css({'top':$('#page-title').height()-20});
-    $('.page-node div.tabs').css({'top':$('#page-title').height()});
+    $('.page-node:not(.page-gallery):not(.page-shop):not(.page-pedia) .flag-outer-bookmarks').css({'top':$('#page-title').height()-20});
+    
+//    $('.page-college .flag-outer-bookmarks').css({'right':$('#page-title').width()+ 5});
+    
+    $('.page-node:not(.page-gallery):not(.page-shop):not(.page-pedia) div.tabs').css({'top':$('#page-title').height()});
+    
+    var prowidth = $('.uc-product-node .field-name-uc-product-image img').width();
+    $('.page-node.page-shop .flag-outer-bookmarks').css({'left':((499-prowidth)/2)+18});
+    
+    /*shop*/
+    $('.node.node-product.view-mode-full .group-left > .display-price,.node.node-product-kit.view-mode-full .group-left > .display-price').prepend('<span>قیمت نهایی:</span>');
+    
+    /*ancher of pedia node*/
+    $('.page-node.page-pedia .view-mode-full .group-anchor > div').prepend('<div class="divanchors"></div>');
+    
+    if($('.page-node.page-pedia .field-type-node-reference').length){
+        $('.page-node.page-pedia .field-type-node-reference').each(function(){
+            var anchclass = $(this).attr('class').split(" ")[1].substr(17);
+            var spananch = $(this).find('.field-label').text().slice(0,-2);
+            
+            $('.divanchors').append('<a class="'+ anchclass +'" href="#'+anchclass+'"><span>'+spananch+'</span></a>');
+            
+            $(this).attr('id', anchclass);
+            
+            
+        });
+    }
+    
+    $('.divanchors a').click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 50
+        }, 700); 
+    });
+  
+       
 });
+
+
+
+
+
+
