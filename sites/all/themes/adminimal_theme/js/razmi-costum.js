b9aaeac0f1f06d9907ef0b546e4364558cdf8e5f
meysamrazmi <m.razmi.92@gmail.com>
2017-05-25 15:51:31 +0430

    update 'add node title to its pictures ald input' functionality

--- sites/all/themes/adminimal_theme/js/razmi-costum.js
+++ sites/all/themes/adminimal_theme/js/razmi-costum.js
@@ -7,9 +7,9 @@ $('.form-textarea-wrapper .grippie').click( function () {
 
 });
 $('.field-name-field-pictures span.fieldset-legend').on('hover' , function () {
-	$('.field-name-field-pictures legend').html($('.field-name-field-pictures legend').html() + '<span class="for-alt">برای پر کردن فیلد های تصاویر <a>کلیک کنید.</a></span>');
+	$('.field-name-field-pictures legend:not(.has-btn)').append('<span class="for-alt">برای پر کردن فیلد های تصاویر <a>کلیک کنید.</a></span>');
 });
-$('.field-name-field-pictures legend').html($('.field-name-field-pictures legend').html() + '<span class="for-alt">برای پر کردن فیلد های تصاویر <a>کلیک کنید.</a></span>');
+$('.field-name-field-pictures legend:not(.has-btn)').append('<span class="for-alt">برای پر کردن فیلد های تصاویر <a>کلیک کنید.</a></span>');
 $('.field-name-field-pictures .for-alt').click(function(){
 	var t = $('#edit-title').val();
 	console.log(t);
@@ -19,7 +19,15 @@ $('.field-name-field-pictures .for-alt').click(function(){
 		$('#edit-field-pictures-und-'+i+'-title').val(t);
 	});	
 });
-
+$('.field-name-field-image .for-alt').click(function(){
+	var t = $('#edit-title').val();
+	console.log(t);
+	$('.field-name-field-image.field-widget-image-image table tbody tr').each(function(i){
+		$('#edit-field-image-und-'+i+'-alt').val(t);
+		console.log($('#edit-field-image-und-'+i+'-alt').val());
+		$('#edit-field-image-und-'+i+'-title').val(t);
+	});	
+});
 $('.ADRES-CONTEINER .POSTI .DATA ,.ADRES-CONTEINER .PHON .DATA').each(function(){
 	$(this).text($(this).text().slice(0,4) +" - "+ $(this).text().slice(4,7) +" - "+ $(this).text().slice(7));
 	$(this).attr('dir' , 'ltr');
@@ -31,11 +39,11 @@ $('.ADRES-CONTEINER .POSTI .DATA ,.ADRES-CONTEINER .PHON .DATA').each(function()
 
 
 /*for firing some scripts after ajax */
-// Drupal.behaviors.adminBehavior = {attach: function (context, settings) {
-// $('.field-name-field-pictures legend').append('<span class="for-alt">برای پر کردن فیلد های تصاویر <a>کلیک کنید.</a></span>');
-
+Drupal.behaviors.adminBehavior = {attach: function (context, settings) {
+$('.field-name-field-pictures legend:not(.has-btn), .field-name-field-image legend:not(.has-btn)').append('<span class="for-alt">برای پر کردن فیلد های تصاویر <a>کلیک کنید.</a></span>');
+$('.field-name-field-pictures legend, .field-name-field-image legend').addClass('has-btn');
 	
 	
 	
-// }};
+}};
 
