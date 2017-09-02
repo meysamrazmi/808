4b86196b63e2f584999117239b4c8a543adecc6e
meysamrazmi <m.razmi.92@gmail.com>
2017-09-02 19:06:21 +0430

    some bug fixing in uc_affiliate2 module and other improvments and styling its pages and adding chart.js file and ...

--- sites/all/modules/uc_affiliate2/uc_affiliate2.js
+++ sites/all/modules/uc_affiliate2/uc_affiliate2.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+(function ($) {
     $('#edit-products-all').click(function() {
         new_value = false;
         
-        if($('#edit-products-all').attr('checked')) {
+        if($('#edit-products-all').is( ":checked" )) {
             new_value = true;    
         }
         
         $('.form-checkbox').each(function() {
-            if(this.id != 'edit-products-all') {
-                $('#' + this.id).attr('checked', new_value);
+            if($(this).attr('id') != 'edit-products-all') {
+                $('#' + $(this).attr('id')).prop('checked', new_value);
             }
         });
     });
-});
+}(jQuery));
