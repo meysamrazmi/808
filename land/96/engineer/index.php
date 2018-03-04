e7be993969d0b4235e0397c9505a7cbaea5620fc
meysam razmi <m.razmi.92@gmail.com>
2018-03-04 18:56:53 +0330

    adding engineer landing page

---
+++ land/96/engineer/index.php
@@ -0,0 +1,170 @@
+	<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="shortcut icon" href="http://civil808.com/sites/default/files/favlogo.png" type="image/png">		
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>جشنواره فروش ویژه 808 به مناسبت روز مهندس</title>
+		<meta name="description" content="جشنواره فروش ویژه 808 به مناسبت روز مهندس">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<meta property="og:title" content="جشنواره فروش ویژه 808 به مناسبت روز مهندس">
+		<meta property="og:type" content="website">
+		<meta property="og:description" content="جشنواره فروش ویژه 808 به مناسبت روز مهندس">
+		<meta property="og:image" content="">
+		<meta property="og:url" content="http://land.civil808.com/96/engineer"> 
+		
+		<link rel="stylesheet" href="./src/main.css">
+		<link rel="stylesheet" href="./src/owl.carousel.min.css">
+		<link rel="stylesheet" href="./src/aos.css">
+	</head>
+
+	<body data-gr-c-s-loaded="true">
+		<ul class="navigat">
+			<li to-id="intro"><span>بالای صفحه</span></li>
+            <li to-id="intro-down"><span>ویژگی های این جشنواره</span></li>
+			<li to-id="latin"><span> رونمایی از سایت لاتین </span></li>
+			<li to-id="aplication"><span> رونمایی از اپلیکیشن 808 </span></li>
+			<li to-id="pbd"><span> گزارش همایش طراحی عملکردی </span></li>
+			<li to-id="footer"><span>پایین صفحه</span></li>
+		</ul>
+		<section class="panel main" id="intro">
+			<div class="inner">
+				<header data-aos="fade-up" data-aos-delay="400">
+					<p class="tagline">روز<span>مهندس</span> مبارک </p>
+					<h1>جشنواره فروش ویژه و رونمایی از بخش های جدید 808</h1>
+					<p style="font-size: 19px;color: #E91E63;color: #f22;font-weight: 500;">به علاوه رونمایی از اپلیکیشن 808 پلاس و سایت لاتین </p>
+				</header>
+				<div class="tarikh" data-aos="fade-up" data-aos-delay="800">
+					<span>5 - 2</span>
+					<span>اسفند ماه</span>
+				</div>	
+				<a href="#steps" class="scroll" data-aos="zoom-out" data-aos-delay="1500" data-aos-anchor-placement="top-bottom"></a>
+			</div>
+		</section>
+
+		<div id="intro-down" style="background: white;padding: 35px;text-align: center;">
+			<p class="desc" data-aos="fade-up" style="font-family: irans;">به همراه <span><span>%</span><span>20</span></span> شارژ هدیه به ازای هر تراکنش</p>
+			<p class="desc" data-aos="fade-up" style="font-family: irans;">و یک ماه عضویت vip به ازای تکمیل تخصص های خود در پروفایل</p>
+			<p data-aos="fade-up" style="font-size: 20px;border: 1px solid #2196F3;margin: 20px auto 0;line-height: 50px;width: 230px;">
+				<span style="display: inline-block;margin: 3px -30px 0 0;color: #2196F3;position: absolute;">+</span> ارسال رایگان
+			</p>
+		</div>
+
+		<section id="latin">
+			<h2 data-aos="zoom-in-up" data-aos-delay="400" class="aos-init aos-animate"> رونمایی از سایت لاتین 808 </h2>
+			<p data-aos="zoom-in-up" data-aos-delay="400" class="aos-init aos-animate" style="text-align: center;	margin-top: 30px;	font-size: 15px;color: #888;"> در این سایت اخبار و مقالات به روز دنیا را به زبان انگلیسی با ما مطالعه کنید. <a href="http://civil808.com/seismosoft" target="_blank"> ورود به سایت </a> <br>آخرین ویدئو های آموزشی زبان انگلیسی تهیه شده توسط موسسه 808 </p>
+			<div class="aks3" data-aos="zoom-out" data-aos-delay="900" class="aboutus aos-init aos-animate">
+				<a href="http://civil808.com/gallery/videos/playlist/3522/18122" target="_blank">
+					<img src="http://civil808.com/sites/default/files/1_2_0.jpg" width="250" height="250">
+				</a>
+				<a href="http://civil808.com/gallery/videos/playlist/3522/18122" target="_blank">
+					<img src="http://civil808.com/sites/default/files/3_16.jpg" width="250" height="250">
+				</a>
+				<a href="http://civil808.com/gallery/videos/playlist/3522/18122" target="_blank">
+					<img src="http://civil808.com/sites/default/files/2_19.jpg" width="250" height="250">
+				</a>
+				<a href="http://civil808.com/node/18212" target="_blank">
+					<img src="http://civil808.com/sites/default/files/styles/uc_product/public/field/image/node_18212.jpg?itok=gUrdM7Q8" width="250" height="250">
+				</a>
+    		</div>
+		</section>
+	
+		<section id="aplication">
+			<div class="wh">
+				<h2 data-aos="zoom-in-up" data-aos-delay="400" class="aos-init aos-animate"> رونمایی از اپلیکیشن اندروید 808 پلاس  </h2>
+				<div data-aos="zoom-out" data-aos-delay="900" class="aboutus aos-init aos-animate">                 
+					<div class="sec-li li">
+						<span class="icon-laptop">
+							<!-- http://land.waterhouse.ir/conf/images/laptop.png -->
+							<img src="./images/cellphone2.png" width="530" height="auto" alt="اپلیکیشن اندروید 808 پلاس" title="اپلیکیشن اندروید 808 پلاس">
+						</span>
+						<div>
+							<p style="font-weight:bold;"> چرا اپلیکیشن 808 پلاس را نصب کنیم؟ </p>
+							<li> مشاهده آخرین اخبار و مقالات تحلیلی وب سایت 808 </li>
+							<li> مشاهده تقویم آموزشی عمران و معماری کشور </li>
+							<li> مشاهده دانشنامه های تخصصی عمران و معماری </li>
+							<li> استفاده از سیستم پرسش و پاسخ 808 </li>
+							<li> امکان ارتباط با مشاوران سایت </li>
+							در حال حاضر می توانید نسخه آزمایشی (بتا) این اپلیکیشن را دانلود کنید.
+							</br>
+							<a class="poster download" href="./src/808plus.apk" target="_self" download="808plus"> دانلود اپلیکیشن اندروید </a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="pbd">
+			<div class="wh">
+				<h2 data-aos="zoom-in-up" data-aos-delay="400" class="aos-init aos-animate"> گزارش برگزاری چهارمین همایش طراحی عملکردی در شهر های مختلف کشور  </h2>
+				<div data-aos="zoom-out" data-aos-delay="900" class="aboutus aos-init aos-animate">                 
+					<div class="sec-li li">
+						<span class="icon-laptop">
+							<!-- http://land.waterhouse.ir/conf/images/laptop.png -->
+							<img src="./images/pbd.jpg" width="530" height="auto" alt="اپلیکیشن اندروید 808 پلاس" title="اپلیکیشن اندروید 808 پلاس">
+						</span>
+						<div>
+							<div>چهارمين همايش طراحي عملكردي امسال در شهر هاي مختلف كشور از ٢٤ تا ٢٩ بهمن به صورت فشرده و پشت سر هم برگزار شد در اين ارتباط آنچه مهم است بيان شود موارد زير است:</div>
+							<ol>
+								<li>
+									اين همايش توسط موسسه ٨٠٨ به طور كاملا مستقل و متاسفانه عليرغم درخواست های همكاری صورت گرفته بدون كوچكترين كمكي از سوی هيچيك از نهاد هاي مرتبط با دانشگاه، نظام مهندسی، انجمن هاي علمی عمرانی وزارت علوم و نهاد های دولتی كشور برگزار شد و حتی در مواردی متاسفانه دست انداز هايی توسط عوامل هريك از نهاد های ذكر شده در برابر برگزاری مستقل اين برنامه انجام گرفت كه ذكر موارد متعدد جزئيات آنرا به بعد موكول ميكنيم
+								</li>
+								<li>
+									استقبال و حضور نزديك به سيصد نَفَر از علاقه مندان به اين موضوع كاملا تخصصی نشان از اراده مهندسينی دارد كه فارغ از هر إلزامي فقط به فراگيری علم جهت اجرا و توسعه كشور عزيزمان می انديشند
+								</li>
+								<li>
+									بعد از تجربه سه دوره همايش طراحي عملكردی از سال ٩٢ از آنجا كه عزم ٨٠٨ حل مسأله مقاوم سازی و پرهيز از ارائه صرفا حرف هاي تكراری تئوری بوده است اينبار برأی اولين بار تجربه برگزاری برنامه ای كاملا كاربردی با تركيب همه سه عامل مورد نياز برأی حل مسأله طراحی عملكردی و مقاوم سازی ساختمان ها از قبيل تئوری دانش ، قابليت نرم افزار و روش های اجرا در كنار هم داشتيم در انتها از تمام همكارانی كه طی اين مدت در حمايت رسانه ای و معنوی اين برنامه به ما كمك كردند تشكر ميكنيم و اميد داريم با هم براي حل معضل ساخت سازه های أيمن برای هم ميهنانمان گام های مؤثر برداريم.<br> مجتبي اصغری ،مدير موسسه ٨٠٨، ٣٠ بهمن١٣٩٦
+								</li>
+							</ol>
+							<a class="poster view" href="http://civil808.com/node/18244" target="_blank"> مشاهده گالری تصاویر </a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+			
+		<div class="footer non-full-height-section" id="footer"><a href="http://civil808.com" target="_blank" class="footer" data-aos-anchor-placement="top-bottom"> بازگشت به سایت</a></div>
+
+
+		<script src="./src/jquery-2.2.1.min.js"></script>
+		<script src="./src/owl.carousel.js"></script>
+		<script src="./src/main.js"></script>
+		<script src="./src/aos.js"></script>
+		<script>
+			AOS.init({
+			offset: 200,
+			duration: 700,
+			easing: 'ease-in-sine',
+			delay: 200,
+			});
+			
+			$('.owl-carousel').owlCarousel({
+				rtl:true,
+				loop:true,
+				margin:20,
+				responsiveClass:true,
+				nav:true,
+				responsive:{
+					0:{
+						items:1,
+					},
+					600:{
+						items:3,
+					},
+					1000:{
+						items:4,
+					}
+				}
+			});
+			
+		</script>
+		<script>  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			
+			ga('create', 'UA-81136025-1', 'auto');
+			ga('send', 'pageview');
+			</script>
+	</body>
+</html>
