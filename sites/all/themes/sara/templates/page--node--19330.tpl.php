62ef9861428b9242e369d13e8c74bcccaf28747a
meysam razmi <m.razmi.92@gmail.com>
2018-07-01 17:05:21 +0430

    ادیت های صفحه اصلی فروشگاه

--- sites/all/themes/sara/templates/page--node--19330.tpl.php
+++ sites/all/themes/sara/templates/page--node--19330.tpl.php
@@ -4,14 +4,18 @@
         </section>
 </header>
 
-<link href=/sites/all/themes/sara/templates/shop/css/app.d428209d46c0236eadc0df1c2e2920ad.css rel=stylesheet>
+<!--<link href=/sites/all/themes/sara/templates/shop/css/app.d428209d46c0236eadc0df1c2e2920ad.css rel=stylesheet>-->
+<link href=/sites/all/themes/sara/templates/shop/css/app.f117245111c4d89cb00984713dd3534d.css rel=stylesheet>
 <div class="tabs">
     <?php print render($tabs); ?>
 </div>
 <div id=app></div>
-<script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/manifest.66a22d3f859f24f8689f.js></script>
+<script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/manifest.2c3090077706dab4effb.js></script>
 <script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/vendor.5e695ee7c20ec2de077f.js></script>
-<script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/app.86e82ae2ae9a34c59b99.js></script>
+<script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/app.23a4f8e9f110d388e94c.js></script>
+<!--<script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/manifest.66a22d3f859f24f8689f.js></script>-->
+<!--<script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/vendor.5e695ee7c20ec2de077f.js></script>-->
+<!--<script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/app.86e82ae2ae9a34c59b99.js></script>-->
 
 
     <footer id="footer">
