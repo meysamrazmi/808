d866dd633d1a2f37c7e9efa5aae97f5dc1af7548
ali <a93banaei@gmail.com>
2018-10-20 12:50:37 +0330

    تکمیل ظاهر صفحه page--remote.tpl اضافه کردن status به جدول لاگ کاربرانی که به صورت ریموت در سایت ما وارد می شوند تعیین وضعیت لاگن هر کاربر به صورت ریموت در بخش های مختلف فرایند لاگین

--- sites/all/themes/sara/templates/page--remote.tpl.php
+++ sites/all/themes/sara/templates/page--remote.tpl.php
@@ -13,7 +13,7 @@
 
 <section id="main">
     <?php print render($page['content']['system_main']); ?>
-    <a href="/user/register" target="_blank" class="join">عضویت در civil808</a>
+    <a href="/user/register" target="_blank" class="join btn-primary">عضویت در civil808</a>
 </section>
 
 <style>
@@ -27,10 +27,48 @@
         max-width: 700px;
         box-shadow: rgba(0,0,0,0.117647) 0 1px 6px, rgba(0,0,0,0.117647) 0 1px 4px;
         overflow: hidden;
-        padding: 15px 15px 50px 15px;
+        padding: 15px 15px 30px 15px;
         margin: auto;
+    }
+    #main .company-name{
+        display: inline-block;
+        font-size: 15px;
+        padding: 10px;
+        vertical-align: top;
+    }
+    #main .company-name label{
+        display: inline;
+        font-size: 15px;
+        padding: 0px 4px;
+    }
+    #main .company-logo{
+        position: absolute;
+        top: 0px;
+        left: 300px;
+    }
+    #main .company-logo img{
+        height: 50px;
+    }
+
+    #main .content .form-item{
+        display: flex;
+        justify-content: space-between;
+        padding: 5px 30px;
+    }
+    #main .content .form-item label{
+        padding: 5px;
+        margin: 5px;
+    }
+    #main .content .form-item input{
+        padding: 5px;
+        margin: 5px;
+    }
+    #main #edit-submit{
+        margin: 10px 30px 0px 30px;
     }
     #main a.join{
         position: absolute;
+        bottom: 20px;
+        right: 310px;
     }
 </style>
