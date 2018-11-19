044de3854deb422d8d08a2801b2cfe0351ef81b6
meysam razmi <m.razmi.92@gmail.com>
2018-11-19 17:02:30 +0330

    added shop and moshavereh vue app builded files

--- sites/all/themes/sara/templates/page--node--19330.tpl.php
+++ sites/all/themes/sara/templates/page--node--19330.tpl.php
@@ -4,14 +4,14 @@
         </section>
 </header>
 
-<link href=/sites/all/themes/sara/templates/shop/css/app.ee12424e24ab3e1c0dfd64f5de0184f8.css rel=stylesheet>
+<link href=/sites/all/themes/sara/templates/shop/css/app.ed7c132e366781b51b072fb946c78577.css rel=stylesheet>
 <div class="tabs">
     <?php print render($tabs); ?>
 </div>
 <div id=app></div>
-<script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/manifest.fd38440cb259b5d0df9f.js></script>
+<script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/manifest.5b7824995943025a7511.js></script>
 <script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/vendor.5e695ee7c20ec2de077f.js></script>
-<script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/app.963d2bf3bbbfc23a68e8.js></script>
+<script type=text/javascript src=/sites/all/themes/sara/templates/shop/js/app.0d5f3a01c1d9f0ba0e18.js></script>
 
     <footer id="footer">
             <div class="container">
