cf20988bcea59a945e08a687f01e468a72bfe2ae
meysam razmi <m.razmi.92@gmail.com>
2018-12-15 19:14:54 +0330

    update main menu

--- sites/all/themes/sara/templates/block--block--58.tpl.php
+++ sites/all/themes/sara/templates/block--block--58.tpl.php
@@ -30,6 +30,7 @@ function render_menu_items($menu, &$level = 0, $sub = false){
 $menu = menu_build_tree('main-menu');
 ?>
 <div id="main-nav">
+    <span class="menu-icon"></span>
     <ul class="main">
         <span class="active-item"></span>
         <a href="/" class="logo" target="_blank"><img src="/sites/all/themes/sara/logo.png" style="height: 50px;padding: 3px;"></a>
@@ -79,9 +80,11 @@ $menu = menu_build_tree('main-menu');
             <a href="/app">اپلیکیشن</a>
         </li>
         <li>
-            <a href="">جستجو</a>
-            <div class="sub">
-                <form class="google-cse" action="/" method="post" id="search-block-form--2" accept-charset="UTF-8"><div><div class="container-inline">
+            <a href="" class="has-sub">جستجو</a>
+            <div class="search sub">
+                <form class="google-cse" action="/" method="post" id="search-block-form--2" accept-charset="UTF-8">
+                    <div>
+                        <div class="container-inline">
                             <h2 class="element-invisible">فرم جستجو</h2>
                             <div class="form-item form-type-textfield form-item-search-block-form">
                                 <label class="element-invisible" for="edit-search-block-form--4">جستجو </label>
@@ -109,6 +112,7 @@ body:not(.uid-3300) #main-nav {
 #main-nav ul.main {
     display: flex;
     position: relative;
+    height: 50px;
 }
 #main-nav ul.main > li > a {
     padding: 0 20px;
@@ -148,9 +152,9 @@ body:not(.uid-3300) #main-nav {
     right: 0;
 }
 #main-nav div.sub.no-mega {
-    width: 250px;
+    width: 300px;
     right: unset;
-    margin-right: -70px;
+    margin-right: -80px;
 }
 #main-nav div.sub.no-mega ul.sub {
     width: 100%;
@@ -182,6 +186,167 @@ span.active-item {
     color: #ccc;
     margin-right: 3px;
 }
+.menu-icon {
+    position: absolute;
+    right: 0;
+    width: 50px;
+    height: 50px;
+    top: 0;
+    display: none;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: all 0.4s ease;
+}
+.menu-icon:before {
+    content: "\f35c";
+    font-family: mat;
+    font-size: 29px;
+    color: #9C27B0;
+}
+#main-nav form {
+    width: 100%;
+    max-width: 700px;
+    margin: auto;
+}
+#main-nav form .container-inline {
+    display: flex;
+}
+#main-nav form .form-item {
+    width: 100%;
+    display: inline-block;
+    margin: auto;
+}
+#main-nav form .form-item input {
+    width: 100%;
+    margin: auto;
+    box-shadow: 0 1px 6px rgba(0,0,0,0.12), 0 1px 4px rgba(0,0,0,0.12);
+    height: 37px;
+    line-height: 36px;
+    font-size: 17px;
+    padding: 0 15px;
+}
+#main-nav form .form-actions {
+    padding-right: 0;
+    margin-right: -1px;
+}
+.search.sub {
+    border-top: 1px solid #eee;
+    background: linear-gradient(to bottom left, #fff ,#eee) !important;
+}
+
+
+@media all and (max-width: 1023px){
+    #main-nav ul.main {
+        width: 300px;
+        height: 100vh;
+        position: fixed;
+        right: -300px;
+        background: #fff;
+        flex-direction: column;
+        z-index: 2;
+        box-shadow: 0 1px 6px rgba(0,0,0,0.12), 0 1px 4px rgba(0,0,0,0.12);
+        transition: right 0.4s;
+        overflow: scroll;
+        overflow: auto;
+        margin-right: -6px;
+    }
+    #main-nav.open ul.main {
+        right: 0px;
+    }
+    .menu-icon {
+        display: flex;
+    }
+    #main-nav a {
+        width: 100%;
+        padding: 0;
+    }
+    #main-nav a:hover {
+        background: #eee;
+    }
+    #main-nav a.has-sub:after {
+        left: 15px;
+        font-size: 18px;
+        color: #888;
+    }
+    #main-nav .open a.has-sub:after {
+        transform: rotate(180deg);
+    }
+    .active-item {
+        display: none;
+    }
+    #main-nav li.active > div.sub {
+        flex-direction: column;
+        padding: 0;
+        box-shadow: inset 0px 3px 4px rgba(0, 0, 0, .175);
+        border: none;
+        background: #f5f5f5;
+    }
+    #main-nav ul.sub {
+        width: 100%;
+        margin: 0;
+    }
+    #main-nav .sub a {
+        padding: 0 25px;
+        line-height: 40px;
+        height: 40px;
+    }
+    #main-nav ul.main > li > a {
+        padding: 0 16px;
+    }
+    #main-nav .sub li:not(.main) {
+        height: 0;
+        overflow: hidden;
+    }
+    #main-nav li.main a {
+        color: #666;
+    }
+    #main-nav div.sub {
+        position: unset;
+        transition: height 0.2s;
+    }
+    #main-nav li.active > div.sub {
+        display: none;
+    }
+    #main-nav li.open > div.sub {
+        display: flex;
+        flex-direction: column;
+        padding: 0;
+        box-shadow: none;
+        border: none;
+        background: #f5f5f5;
+    }
+    a.logo {
+        text-align: center;
+        padding: 10px 5px 0;
+        border-bottom: 1px solid #eee;
+        background: linear-gradient(to bottom left, #673AB7 ,#9C27B0) !important;
+    }
+    a.logo img {
+        filter: brightness(0) invert(1);
+        height: 70px !important;
+    }
+    div#main-nav.open .menu-icon {
+        right: 294px;
+        background: #420240;
+        z-index: 4;
+    }
+    div#main-nav.open .menu-icon:before {
+        color: #fff;
+        content: "\f156";
+    }
+    #main-nav div.sub.no-mega {
+        width: 100%;
+        margin: 0;
+    }
+    #main-nav form .form-actions {
+        padding: 5px 0px 0;
+    }
+    form#search-block-form--2 .container-inline {
+        flex-direction: column;
+        padding: 5px;
+    }
+}
 </style>
 
 <script>
@@ -201,6 +366,14 @@ $(document).ready(function () {
 		el.removeClass('active')
 		el.removeClass('hover')
 	})
+
+    $('a.has-sub').click(function(e){
+    	e.preventDefault()
+        $(this).parent().toggleClass('open')
+    })
+    $('.menu-icon').click(function(){
+    	$(this).parent().toggleClass('open')
+    })
 })
 </script>
 
