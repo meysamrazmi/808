f12b43b968ec10e2fc2274bd88b45604b9d87bce
ali <=>
2019-01-13 11:55:17 +0330

    check in information ajax for each tag if tag is not expert, it omits the users

--- sites/all/themes/sara/js/question-pages.js
+++ sites/all/themes/sara/js/question-pages.js
@@ -26,7 +26,8 @@ $(document).ready(function(){
                 type: 'GET',
                 url: '/experts/information/' + tid,
                 success: function(result){
-                    element.next('.information').html('<div class="pic"><image src="'+ result['pic'] +'"/></div><div class="data"><div class="q-count">  سوالات این تخصص : ' + result['question_count'] + ' </div><div class="u-count"> کاربران این تخصص : ' + result['user_count'] + '</div><a href="/taxonomy/term/' + result['tid'] + ' " target="_blank">دانشنامه</a></div>');
+                    if(result['expert'] === true) element.next('.information').html('<div class="pic"><image src="'+ result['pic'] +'"/></div><div class="data"><div class="q-count">  سوالات این تخصص : ' + result['question_count'] + ' </div><div class="u-count"> کاربران این تخصص : ' + result['user_count'] + '</div><a href="/taxonomy/term/' + result['tid'] +' " target="_blank">بیشتر...</a></div>');
+                    else element.next('.information').html('<div class="pic"><image src="'+ result['pic'] +'"/></div><div class="data"><div class="q-count">  سوالات این تگ : ' + result['question_count'] + ' </div><a href="/taxonomy/term/' + result['tid'] +' " target="_blank">بیشتر...</a></div>');
                     element.next('.information').addClass("filled");
                     
                 },
